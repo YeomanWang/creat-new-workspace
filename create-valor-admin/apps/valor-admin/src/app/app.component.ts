30cbac03271397c5a35a3654bb1cea6e88671590
yeoman wang <yuming.wang423@gmail.com>
2021-11-29 15:26:11 +0800

    build: first commit

---
+++ create-valor-admin/apps/valor-admin/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'create-valor-admin-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'valor-admin';
+}
